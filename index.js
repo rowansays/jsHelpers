d8ff2cc0e987670aaa38cf386b8aacded75684b7
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-21 03:11:57 -0800

    Adding an index file

---
+++ index.js
@@ -0,0 +1,4 @@
+export { castArray } from './castArray.js'
+export { castString } from './castString.js'
+export { castStringArray } from './castStringArray.js'
+export { isIterable } from './isIterable.js'

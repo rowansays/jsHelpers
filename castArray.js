c5dc7b7db87be8d30ecf912816dda35c993ff9cc
rowansays <74547960+rowansays@users.noreply.github.com>
2020-12-23 06:15:39 -0800

    castArray: allow to be called in recursive mode. update tests

--- castArray.js
+++ castArray.js
@@ -1,37 +1,59 @@
+import { isIterableObject } from './isIterableObject.js'
+
 /**
  * Cast a value to an array
  *
+ * This function is mutually recursive with maybePush().
+ *
  * @param {*} aught - The value to cast.
- * @param {function} func - A function to be called for every item. When this
+ * @param {function} [filter] - A function to be called for every item. When this
  *   function returns undefined the value will not be included in the output
  *   array.
+ * @param {string} [mode] - When "recursive" castArray() will iterate through
+ *   all iterable values and extract any value that passes the filter.
  *
  * @return {Array}
  */
-export function castArray (aught, filter) {
-  // Wrap scalar values in arrays.
-  aught = typeof aught === 'object' ? aught : [aught]
+export function castArray (aught, filter, mode) {
+  if (typeof aught === 'undefined') {
+    return []
+  }
 
-  // Null objects become empty arrays.
-  aught = aught === null ? [] : aught
+  // Wrap all other scalar values in arrays.
+  aught = typeof aught === 'object' ? aught : [aught]
 
   // Choose looping strategy.
   const output = []
-  if (typeof aught.forEach === 'function') {
-    aught.forEach(x => { maybePush(filter, output, x) })
-  } else if (Symbol.iterator in Object(aught)) {
-    for (const x of aught) { maybePush(filter, output, x) }
+  if (!!aught && typeof aught.forEach === 'function') {
+    aught.forEach(x => { maybePush(filter, output, x, mode) })
+  } else if (isIterableObject(aught)) {
+    for (const x of aught) { maybePush(filter, output, x, mode) }
   } else {
-    for (const x in aught) { maybePush(filter, output, aught[x]) }
+    maybePush(filter, output, aught, mode)
   }
 
   return output
 }
 
-function maybePush (filter, array, value) {
-  filter = typeof filter === 'function' ? filter : x => x
-  const clean = filter(value, array)
-  if (typeof clean !== 'undefined') {
-    array.push(clean)
+function isLoopable (aught) {
+  return !!aught && typeof aught.forEach === 'function' && isIterableObject(aught)
+}
+
+/**
+ * This function is mutually recursive with castArray().
+ */
+function maybePush (filter, array, value, mode) {
+  const defaultFilter = x => x
+  filter = typeof filter === 'function' ? filter : defaultFilter
+
+  if (filter !== defaultFilter) {
+    const filteredValue = filter(value, array)
+    if (typeof filteredValue !== 'undefined') {
+      array.push(filteredValue)
+    } else if (mode === 'recursive' && isLoopable(value)) {
+      array.push(...castArray(value, filter, mode))
+    }
+  } else {
+    array.push(value)
   }
 }
--- test/castArray.unit.test.js
+++ test/castArray.unit.test.js
@@ -1,145 +1,285 @@
 import { castArray } from '../castArray.js'
-import { mockIterableXyz } from './mocks/iterable.js'
+import {
+  mockIterableMixed,
+  mockIterableNamedObjects,
+  mockIterableIntegers,
+  mockIterableXyz
+} from './mocks/iterable.js'
 
-describe('castArray() - Unit Tests', () => {
-  test('it is a function.', () => {
+describe('castArray', () => {
+  it('is a function.', () => {
     expect(typeof castArray).toEqual('function')
   })
-  describe('Parameter 1 - aught {*}', () => {
-    describe('it wraps scalar values', () => {
-      test('boolean true', () => {
-        const target = [true]
-        const casted = castArray(true)
-        expect(casted).toEqual(target)
-        expect(casted).not.toBe(target)
-      })
-      test('boolean false', () => {
-        const target = [false]
-        const casted = castArray(false)
-        expect(casted).toEqual(target)
-        expect(casted).not.toBe(target)
-      })
-      test('empty string', () => {
-        const target = ['']
-        const casted = castArray('')
-        expect(casted).toEqual(target)
-        expect(casted).not.toBe(target)
-      })
+})
+describe('castArray()', () => {
+  it('returns an empty array.', () => {
+    expect(castArray()).toEqual([])
+  })
+})
+describe('castArray(aught)', () => {
+  it('returns empty array for undefined', () => {
+    expect(castArray()).toMatchObject([])
+  })
+  it('wraps booleans in an array', () => {
+    expect(castArray(true)).toMatchObject([true])
+    expect(castArray(false)).toMatchObject([false])
+  })
+  it('wraps numbers in an array', () => {
+    expect(castArray(NaN)).toMatchObject([NaN])
+    expect(castArray(Infinity)).toMatchObject([Infinity])
+    expect(castArray(1234)).toMatchObject([1234])
+    expect(castArray(1.234)).toMatchObject([1.234])
+  })
+  it('wraps strings in an array', () => {
+    expect(castArray('')).toMatchObject([''])
+    expect(castArray('abc')).toMatchObject(['abc'])
+  })
+  it('wraps a bigint in an array', () => {
+    expect(castArray(1234n)).toMatchObject([1234n])
+  })
+  it('wraps a symbol in an array', () => {
+    const s = Symbol()
+    expect(castArray(s)).toMatchObject([s])
+  })
+  it('wraps null in an array', () => {
+    expect(castArray(null)).toMatchObject([null])
+  })
+  describe('Array', () => {
+    it('creates an new empty array from an empty array', () => {
+      const a = []
+      const b = castArray(a)
+      expect(b).not.toBe(a)
+      expect(b).toMatchObject(a)
     })
-    describe('it returns an empty array for the following values', () => {
-      const target = []
-      test('undefined', () => {
-        expect(castArray(undefined)).toEqual(target)
-      })
-      test('null', () => {
-        expect(castArray(null)).toEqual(target)
-      })
-      test('empty plain object', () => {
-        expect(castArray({})).toEqual(target)
-      })
-      test('empty array', () => {
-        expect(castArray([])).toEqual(target)
-      })
-      test("Array with a value of undefined", () => {
-        expect(castArray([undefined])).toEqual(target)
-      })
-      test('empty Set instance', () => {
-        expect(castArray(new Set())).toEqual(target)
-      })
-      test('empty Map instance', () => {
-        expect(castArray(new Map())).toEqual(target)
-      })
+    it('extracts undefined values into a new array', () => {
+      const a = [undefined, undefined, undefined]
+      const b = castArray(a)
+      expect(b).not.toBe(a)
+      expect(b).toMatchObject(a)
     })
-    describe('it returns a copy of input array', () => {
-      test("[null]", () => {
-        const value = [null]
-        const casted = castArray(value)
-        expect(casted).toEqual(value)
-        expect(casted).not.toBe(value)
-      })
-      test("[true]", () => {
-        const value = [true]
-        const casted = castArray(value)
-        expect(casted).toEqual(value)
-        expect(casted).not.toBe(value)
-      })
-      test("[false]", () => {
-        const value = [false]
-        const casted = castArray(value)
-        expect(casted).toEqual(value)
-        expect(casted).not.toBe(value)
-      })
-      test("['a']", () => {
-        const value = ['a']
-        const casted = castArray(value)
-        expect(casted).toEqual(value)
-        expect(casted).not.toBe(value)
-      })
-      test("[1]", () => {
-        const value = [1]
-        const casted = castArray(value)
-        expect(casted).toEqual(value)
-        expect(casted).not.toBe(value)
-      })
+    it('extracts boolean values into a new array', () => {
+      const a = [true, false, true, false]
+      const b = castArray(a)
+      expect(b).not.toBe(a)
+      expect(b).toMatchObject(a)
     })
-    describe('it extracts the values of other objects', () => {
-      test("iterable objects", () => {
-        const value = mockIterableXyz
-        expect(castArray(value)).toEqual(['x', 'y', 'z'])
-      })
-      test("{ a: 1, b: 2, c: 3 }", () => {
-        const value = { a: 1, b: 2, c: 3 }
-        expect(castArray(value)).toEqual([1, 2, 3])
-      })
-      test("new Set([1, 2, 3])", () => {
-        const value = new Set([1, 2, 3])
-        expect(castArray(value)).toEqual([1, 2, 3])
-      })
-      test("new Map([['a', 1], ['b', 2], ['c', 3]])", () => {
-        const value = new Map([['a', 1], ['b', 2], ['c', 3]])
-        expect(castArray(value)).toEqual([1, 2, 3])
-      })
+    it('extracts numeric values into a new array', () => {
+      const a = [123, 1.23, NaN, Infinity]
+      const b = castArray(a)
+      expect(b).not.toBe(a)
+      expect(b).toMatchObject(a)
+    })
+    it('extracts bigints into a new array', () => {
+      const a = [1n, 2n ,3n]
+      const b = castArray(a)
+      expect(b).not.toBe(a)
+      expect(b).toMatchObject(a)
+    })
+    it('extracts symbols into a new array', () => {
+      const s1 = Symbol()
+      const s2 = Symbol()
+      const s3 = Symbol()
+      const a = [s1, s2, s3]
+      const b = castArray(a)
+      expect(b).not.toBe(a)
+      expect(b[0]).toBe(s1)
+      expect(b[1]).toBe(s2)
+      expect(b[2]).toBe(s3)
+    })
+    it('extracts null values into a new array', () => {
+      const a = [null, null, null]
+      const b = castArray(a)
+      expect(b).not.toBe(a)
+      expect(b).toMatchObject(a)
+    })
+    it('extracts strings into a new array', () => {
+      const a = ['a', 'b', 'c']
+      const b = castArray(a)
+      expect(b).not.toBe(a)
+      expect(b).toMatchObject(a)
+    })
+    it('extracts references to arrays into a new array', () => {
+      const o1 = [1, 2, 3]
+      const o2 = [4, 5, 6]
+      const o3 = [7, 8, 9]
+      const a = [o1, o2, o3]
+      const b = castArray(a)
+      expect(b).not.toBe(a)
+      expect(b[0]).toBe(o1)
+      expect(b[1]).toBe(o2)
+      expect(b[2]).toBe(o3)
+    })
+    it('extracts references to object literals into a new array', () => {
+      const o1 = { name: 'All work and' }
+      const o2 = { name: 'no play makes' }
+      const o3 = { name: 'jack a dull boy' }
+      const a = [o1, o2, o3]
+      const b = castArray(a)
+      expect(b).not.toBe(a)
+      expect(b[0]).toBe(o1)
+      expect(b[1]).toBe(o2)
+      expect(b[2]).toBe(o3)
     })
   })
-  describe('Parameter 2 - filter {function}', () => {
-    test("return values of undefined always produces an empty array", () => {
-      const value = ['a', 1, false, true]
-      const filter = () => undefined
-      expect(castArray(value, filter)).toEqual([])
+  describe('Set', () => {
+    it('returns an empty array for an empty Set', () => {
+      const a = new Set()
+      const b = castArray(a)
+      expect(b).toMatchObject([])
     })
-    test("can overwrite all values with a custom value", () => {
-      const value = ['a', 1, false, true]
-      const filter = () => 'hi'
-      expect(castArray(value, filter)).toEqual(['hi', 'hi', 'hi', 'hi'])
+    it('extracts numeric values', () => {
+      const a = new Set([1, 2, 3])
+      const b = castArray(a)
+      expect(b).toMatchObject([1, 2, 3])
     })
-    test("accepts the values from the input", () => {
-      const value = ['a', 1, false, true]
-      const filter = (value) => value
-      expect(castArray(value, filter)).toEqual(['a', 1, false, true])
+    it('extracts string values', () => {
+      const a = new Set(['a', 'b', 'c'])
+      const b = castArray(a)
+      expect(b).toMatchObject(['a', 'b', 'c'])
     })
-    test('accepts a "value" parameter from the iterating the input', () => {
-      const value = ['a', 1, false, true]
-      const filter = (value) => value
-      expect(castArray(value, filter)).toEqual(['a', 1, false, true])
+    it('extracts object literal references', () => {
+      const o1 = { name: 'All work and' }
+      const o2 = { name: 'no play makes' }
+      const o3 = { name: 'jack a dull boy' }
+      const a = new Set([o1, o2, o3])
+      const b = castArray(a)
+      expect(b[0]).toBe(o1)
+      expect(b[1]).toBe(o2)
+      expect(b[2]).toBe(o3)
     })
-    test('can be used to filter by type:string', () => {
-      const value = ['a', 1, false, true]
-      const filter = (value) => typeof value === 'string' ? value : undefined
-      expect(castArray(value, filter)).toEqual(['a'])
+  })
+  describe('Map', () => {
+    it('returns an empty array for an empty Map', () => {
+      const a = new Map()
+      const b = castArray(a)
+      expect(b).toMatchObject([])
     })
-    test('can be used to filter by type:number', () => {
-      const value = ['a', 1, false, true]
-      const filter = (value) => typeof value === 'number' ? value : undefined
-      expect(castArray(value, filter)).toEqual([1])
+    it('extracts numeric values', () => {
+      const a = new Map([['a', 1], ['b', 2], ['c', 3]])
+      const b = castArray(a)
+      expect(b).toMatchObject([1, 2, 3])
     })
-    test('can be used to filter by type:boolean', () => {
-      const value = ['a', 1, false, true]
-      const filter = (value) => typeof value === 'boolean' ? value : undefined
-      expect(castArray(value, filter)).toEqual([false, true])
+    it('extracts string values', () => {
+      const a = new Map([['a', 'a'], ['b', 'b'], ['c', 'c']])
+      const b = castArray(a)
+      expect(b).toMatchObject(['a', 'b', 'c'])
     })
-    test('has no effect when not a function', () => {
-      const value = ['a', 1, false, true]
-      expect(castArray(value, 'I am a string')).toEqual(value)
+    it('extracts object literal references', () => {
+      const o1 = { name: 'All work and' }
+      const o2 = { name: 'no play makes' }
+      const o3 = { name: 'jack a dull boy' }
+      const a = new Map([ ['a', o1], ['b', o2], ['c', o3] ])
+      const b = castArray(a)
+      expect(b[0]).toBe(o1)
+      expect(b[1]).toBe(o2)
+      expect(b[2]).toBe(o3)
     })
+  })
+  describe('Iterable', () => {
+    it('extracts numeric values', () => {
+      const a = castArray(mockIterableIntegers)
+      expect(a).toMatchObject([1, 2, 3])
+    })
+    it('extracts string values', () => {
+      const a = castArray(mockIterableXyz)
+      expect(a).toMatchObject(['x', 'y', 'z'])
+    })
+    it('extracts plain objects', () => {
+      const a = castArray(mockIterableNamedObjects)
+      expect(a).toMatchObject([
+        { name: 'Kitten' },
+        { name: 'Bunny' },
+        { name: 'Spider' }
+      ])
+    })
+  })
+})
+describe('castArray(aught, filter)', () => {
+  it('no value is saved when filter returns undefined', () => {
+    const value = ['a', 1, false, true]
+    const removeEverything = () => undefined
+    const removeA = (x) => x === 'a' ? undefined : x
+    const remove1 = (x) => x === 1 ? undefined : x
+    const removeF = (x) => x === false ? undefined : x
+    const removeT = (x) => x === true ? undefined : x
+    expect(castArray(value, removeEverything)).toEqual([])
+    expect(castArray(value, removeA)).toEqual([1, false, true])
+    expect(castArray(value, remove1)).toEqual(['a', false, true])
+    expect(castArray(value, removeF)).toEqual(['a', 1, true])
+    expect(castArray(value, removeT)).toEqual(['a', 1, false])
+  })
+  it('can overwrite all values with a custom value', () => {
+    const value = ['a', 1, false, true]
+    const filter = () => 'hi'
+    expect(castArray(value, filter)).toEqual(['hi', 'hi', 'hi', 'hi'])
+  })
+  it('accepts the values from the input', () => {
+    const value = ['a', 1, false, true]
+    const filter = (value) => value
+    expect(castArray(value, filter)).toEqual(['a', 1, false, true])
+  })
+  it('can be used to filter by type:string', () => {
+    const value = ['a', 1, false, true]
+    const filter = (value) => typeof value === 'string' ? value : undefined
+    expect(castArray(value, filter)).toEqual(['a'])
+  })
+  it('can be used to filter by type:number', () => {
+    const value = ['a', 1, false, true]
+    const filter = (value) => typeof value === 'number' ? value : undefined
+    expect(castArray(value, filter)).toEqual([1])
+  })
+  it('can be used to filter by type:boolean', () => {
+    const value = ['a', 1, false, true]
+    const filter = (value) => typeof value === 'boolean' ? value : undefined
+    expect(castArray(value, filter)).toEqual([false, true])
+  })
+  it('has no effect when not a function', () => {
+    const value = ['a', 1, false, true]
+    expect(castArray(value, undefined)).toEqual(value)
+    expect(castArray(value, null)).toEqual(value)
+    expect(castArray(value, false)).toEqual(value)
+    expect(castArray(value, true)).toEqual(value)
+    expect(castArray(value, 'abc')).toEqual(value)
+    expect(castArray(value, 12345)).toEqual(value)
+    expect(castArray(value, 1234n)).toEqual(value)
+    expect(castArray(value, Symbol())).toEqual(value)
+    expect(castArray(value, {})).toEqual(value)
+    expect(castArray(value, [])).toEqual(value)
+    expect(castArray(value, new Map())).toEqual(value)
+    expect(castArray(value, new Set())).toEqual(value)
+  })
+  it('does not recurse', () => {
+    const a = [1, 2, 3, 'a', 'b', ['c']]
+    const b = castArray(a, x => typeof x === 'string' ? x : undefined)
+    expect(b.length).toEqual(2)
+    expect(b[0]).toEqual('a')
+    expect(b[1]).toEqual('b')
+  })
+})
+describe('castArray(aught, filter, mode)', () => {
+  it('walks nested arrays to find strings', () => {
+    const a = [
+      [1, 2n, 3.0, ['Hello']],
+      [1, 2n, 3.0, [['World']]],
+      [1, 2n, 3.0, [[['I\'m your wild girl']]]],
+    ]
+    const filter = x => typeof x === 'string' ? x : undefined
+    const b = castArray(a, filter, 'recursive')
+    expect(b[0]).toEqual('Hello')
+    expect(b[1]).toEqual('World')
+    expect(b[2]).toEqual('I\'m your wild girl')
+  })
+  it('walks nested sets to find strings', () => {
+    const a = new Set([
+      new Set( [1, 2n, 3.0, new Set(['Hello']) ]),
+      new Set( [1, 2n, 3.0, new Set(['World']) ]),
+      new Set( [1, 2n, 3.0, new Set(['I\'m your wild girl']) ]),
+    ])
+
+    const filter = x => typeof x === 'string' ? x : undefined
+    const b = castArray(a, filter, 'recursive')
+    expect(b[0]).toEqual('Hello')
+    expect(b[1]).toEqual('World')
+    expect(b[2]).toEqual('I\'m your wild girl')
   })
 })
